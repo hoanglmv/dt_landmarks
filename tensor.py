53b0017c76354f59aefb99a82151fb22f4b19898
hoanglmv <131538387+hoanglmv@users.noreply.github.com>
2025-04-19 11:21:53 +0700

    update

--- tensor.py
+++
@@ -1,7 +0,0 @@
-import cv2
-from src.dataset.FaceLandmarksDataset import FaceLandmarksDataset
-dataset = FaceLandmarksDataset()
-image, landmarks = dataset[0]  
-print(image.shape)
-print(landmarks)
-
